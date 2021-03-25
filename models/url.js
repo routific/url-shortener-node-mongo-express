51d430308834def4e605fd22b93a93c36b02f621
Scott J <scott@routific.com>
2021-03-25 08:08:19 -0700

    Adding support for sharded cluster

--- models/url.js
+++ models/url.js
@@ -5,7 +5,13 @@ const Schema = mongoose.Schema;
 let urlSchema = new Schema({
   long_url: String,
   short_id: String,
-  created_at: Date
+  created_at: Date,
+  location: String
+}, {
+  shardKey: {
+    _id: 1,
+    location: 1
+  }
 });
 // create indexes
 urlSchema.index({ long_url: 1 });
@@ -14,6 +20,7 @@ urlSchema.index({ short_id: 1 });
 urlSchema.pre('save', function(next){
   let doc = this;
   doc.created_at = new Date();
+  doc.location = 'US';
   next();
 });
 
